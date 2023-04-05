5f2a79bba8c5b00ab1cc128b13a3638799e62b3f
elimend <128734627+elimend@users.noreply.github.com>
2023-04-04 22:58:32 -0400

    Basic nested if-else

---
+++ nested_if-else.java
@@ -0,0 +1,10 @@
+int grade = 85;
+if (grade >= 90) {
+    System.out.println("A grade");
+} else if (grade >= 80) {
+    System.out.println("B grade");
+} else if (grade >= 70) {
+    System.out.println("C grade");
+} else {
+    System.out.println("Fail");
+}
